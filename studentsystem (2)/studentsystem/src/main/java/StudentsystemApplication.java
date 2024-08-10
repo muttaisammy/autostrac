9050ba5dddb2dff2db595c2edf8c00f5faf76f6f
muttaisammy <muttaisammy@gmail.com>
2024-08-10 13:20:27 +0300

    m

---
+++ studentsystem (2)/studentsystem/src/main/java/StudentsystemApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsystemApplication.class, args);
+    }
+
+}
