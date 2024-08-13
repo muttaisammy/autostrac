cac3d6c4828ea496053ab72a94ae3c89ccc928ff
muttaisammy <muttaisammy@gmail.com>
2024-08-13 11:43:40 +0300

    m

---
+++ autostrac/src/main/java/com/autostrac/autostrac/AutostracApplication.java
@@ -0,0 +1,13 @@
+package com.autostrac.autostrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutostracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutostracApplication.class, args);
+	}
+
+}
---
+++ autostrac/src/test/java/com/autostrac/autostrac/AutostracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autostrac.autostrac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutostracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
